d21c8910aed644b66ce5dcaca8bc20706a39806e
Dakota Brown <dakotabrown@macbook-pro-25.local>
2018-09-09 20:18:19 -0500

    Initial Commit

---
+++ simpleEncryptionTest/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  simpleEncryptionTest
+//
+//  Created by Dakota Brown on 9/9/18.
+//  Copyright © 2018 Dakota Brown. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
