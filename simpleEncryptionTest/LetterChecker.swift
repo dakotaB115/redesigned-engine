0687040328cd269d36fc5a839504b3b2a16d70f7
Dakota Brown <dakotabrown@bcms_15407.barren.ketsds.net>
2018-09-10 12:48:10 -0500

    made a simple letter to number converstion

---
+++ simpleEncryptionTest/LetterChecker.swift
@@ -0,0 +1,68 @@
+//
+//  LetterChecker.swift
+//  simpleEncryptionTest
+//
+//  Created by Dakota Brown on 9/9/18.
+//  Copyright © 2018 Dakota Brown. All rights reserved.
+//
+
+import Foundation
+
+func letterChecker() {
+    var numberEncypt = 0
+    for letter in string {
+        if letter == "a" {
+            numberEncypt += 1
+        } else if letter == "b" {
+            numberEncypt += 2
+        } else if letter == "c" {
+            numberEncypt += 3
+        } else if letter == "d" {
+            numberEncypt += 4
+        } else if letter == "e" {
+            numberEncypt += 5
+        } else if letter == "f" {
+            numberEncypt += 6
+        } else if letter == "g" {
+            numberEncypt += 7
+        } else if letter == "h" {
+            numberEncypt += 8
+        } else if letter == "i" {
+            numberEncypt += 9
+        } else if letter == "j" {
+            numberEncypt += 10
+        } else if letter == "k" {
+            numberEncypt += 11
+        } else if letter == "l" {
+            numberEncypt += 12
+        } else if letter == "m" {
+            numberEncypt += 13
+        } else if letter == "n" {
+            numberEncypt += 14
+        } else if letter == "o" {
+            numberEncypt += 15
+        } else if letter == "p" {
+            numberEncypt += 16
+        } else if letter == "q" {
+            numberEncypt += 17
+        } else if letter == "r" {
+            numberEncypt += 18
+        } else if letter == "s" {
+            numberEncypt += 19
+        } else if letter == "t" {
+            numberEncypt += 20
+        } else if letter == "u" {
+            numberEncypt += 21
+        } else if letter == "v" {
+            numberEncypt += 22
+        } else if letter == "w" {
+            numberEncypt += 23
+        } else if letter == "x" {
+            numberEncypt += 24
+        } else if letter == "y" {
+            numberEncypt += 25
+        } else if letter == "z" {
+            numberEncypt += 26
+        }
+    }
+}
--- simpleEncryptionTest/main.swift
+++ simpleEncryptionTest/main.swift
@@ -8,5 +8,8 @@
 
 import Foundation
 
-print("Hello, World!")
+let string = "@jinsai"
 
+let array = [Int]()
+
+letterChecker()
